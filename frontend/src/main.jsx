7c4b7fe008f81feb01b5bd8afb243c5867bf0780
stk1201 <satokichi121@gmail.com>
2024-09-10 22:47:44 +0900

    git

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import Explanation from './pages/Explanation';
+import Main from './pages/Main';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ChakraProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/explanation" element={<Explanation />} />
+        {/* 他のルートがあればここに追加 */}
+      </Routes>
+    </Router>
+    </ChakraProvider>
   </StrictMode>,
 )
