90ae158db89b1dfa32d5c2aef81dae674eafa549
stk1201 <satokichi121@gmail.com>
2024-09-12 17:51:11 +0900

    api準備完了

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,25 +1,7 @@
-import requests
-import json
-import os
-from dotenv import load_dotenv
+from rest_framework import viewsets
+from .models import JobListing
+from .serializers import JobListingSerializer
 
-load_dotenv()
-
-
-Lowest_price = input("最低価格 ----> ")
-Highest_price = input("最高価格 ----> ")
-item_name = input('キーワード-->')
-
-appid = "(os)"
-url = 'https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch?appid={}&price_from={}&price_to={}&query={}&results=100'.format(appid, Lowest_price, Highest_price, item_name)
-call = requests.get(url)
-res_dict = json.loads(call.content)
-count = len(res_dict['hits'])
-
-for i in range(count):
-   print("商品名：", res_dict['hits'][i]['name'])
-   print("説明：", res_dict['hits'][i]['description'])
-   print("価格：", res_dict['hits'][i]['price'])
-   print("商品コード：", res_dict['hits'][i]['code'])
-   print("JANコード：", res_dict['hits'][i]['janCode'])
-   print("商品URL", res_dict['hits'][i]['url'])
+class JobListingViewSet(viewsets.ModelViewSet):
+   queryset = JobListing.objects.all()
+   serializer_class = JobListingSerializer
